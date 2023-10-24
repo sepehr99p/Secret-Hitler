32c601f2e8326429508a289f20e244e78e3503df
sepehr99p <sepehr.pourekrami@gmail.com>
2023-10-24 17:01:16 +0330

    optimize launcherActivity

--- app/src/main/java/com/example/secrethitler/ui/launcher/LauncherActivity.kt
+++ app/src/main/java/com/example/secrethitler/ui/launcher/LauncherActivity.kt
@@ -4,19 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.secrethitler.R
 import com.example.secrethitler.databinding.ActivityLauncherBinding
 import com.example.secrethitler.ui.MainActivity
 
 class LauncherActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityLauncherBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,8 +19,14 @@ class LauncherActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-
-        Handler(Looper.getMainLooper()).postDelayed({startActivity(Intent(this,MainActivity::class.java))},3000)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(
+                Intent(
+                    this,
+                    MainActivity::class.java
+                )
+            )
+        }, 3000)
 
     }
 
